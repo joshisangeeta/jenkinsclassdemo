57d29e20c53e1937f9194b01aa21f7df9d3fec8d
sangeeta joshi <sangeeta.joshi2@gmail.com>
2020-11-09 12:07:53 +0530

    first commit

---
+++ gitclassdemo/src/main/java/com/sj/GitclassdemoApplication.java
@@ -0,0 +1,13 @@
+package com.sj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitclassdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitclassdemoApplication.class, args);
+	}
+
+}
---
+++ gitclassdemo/src/main/java/com/sj/controller/WebController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.sj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author HP
+ *
+ */
+@RestController
+public class WebController {
+
+	@GetMapping("/hi")
+	public String sayHi() {
+		return "Hello world";
+	}
+	
+	
+}
